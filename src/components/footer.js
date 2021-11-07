72689ba0a9736ab542a7a59f84cc82613ead20a3
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-11-06 21:05:53 -0700

    footer fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://github.com/michaelyaacoub/portfolio')
+    fetch('https://github.com/michaelyaacoub')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
